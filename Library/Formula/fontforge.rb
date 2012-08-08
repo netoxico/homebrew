a93fe390b0d6035d701ad79a09f3d50227fe0bce
Ernesto Vargas <me@netoxico.com>
2012-08-08 16:49:51 -0500

    patch for installing fontforge in montain_lion and xcode vercion 4.4

--- Library/Formula/fontforge.rb
+++ Library/Formula/fontforge.rb
@@ -1,34 +1,59 @@
 require 'formula'
 
 class Fontforge < Formula
-  url 'http://downloads.sourceforge.net/project/fontforge/fontforge-source/fontforge_full-20110222.tar.bz2'
-  head 'git://fontforge.git.sourceforge.net/gitroot/fontforge/fontforge'
   homepage 'http://fontforge.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/fontforge/fontforge-source/fontforge_full-20110222.tar.bz2'
   md5 '5be4dda345b5d73a27cc399df96e463a'
+
+  head 'git://fontforge.git.sourceforge.net/gitroot/fontforge/fontforge'
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
   depends_on 'pango'
   depends_on 'potrace'
+  depends_on :x11
 
   def options
     [['--without-python', 'Build without Python.']]
   end
 
-  fails_with_llvm "Compiling cvexportdlg.c fails with error: initializer element is not constant"
+  fails_with :llvm do
+    build 2336
+    cause "Compiling cvexportdlg.c fails with error: initializer element is not constant"
+  end
 
   def install
-    args = ["--prefix=#{prefix}", "--enable-double", "--without-freetype-bytecode"]
+    args = ["--prefix=#{prefix}",
+            "--enable-double",
+            "--without-freetype-bytecode"]
+
     args << "--without-python" if ARGV.include? "--without-python"
 
-    ENV.x11
+	# Using 10.8? Need to compile against the 10.7 SDK
+	if MacOS.mountain_lion? or MacOs::Xcode.version >= '4.4'
+		lion_prefix = File.join(MacOS::Xcode.prefix, "Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk")
+		ENV.append "CFLAGS", "-isysroot " + lion_prefix
+	end
     # Fix linker error; see: http://trac.macports.org/ticket/25012
     ENV.append "LDFLAGS", "-lintl"
     system "./configure", *args
 
+    # Fix hard-coded install locations that don't respect the target bindir
     inreplace "Makefile" do |s|
       s.gsub! "/Applications", "$(prefix)"
-      s.gsub! "/usr/local/bin", "$(bindir)"
+      s.gsub! "ln -s /usr/local/bin/fontforge", "ln -s $(bindir)/fontforge"
+    end
+
+    # Fix hard-coded include file paths. Reported usptream:
+    # http://sourceforge.net/mailarchive/forum.php?thread_name=C1A32103-A62D-468B-AD8A-A8E0E7126AA5%40smparkes.net&forum_name=fontforge-devel
+    # https://trac.macports.org/ticket/33284
+    header_prefix = MacOS::Xcode.prefix
+    # Using 10.8? More hardcoded paths to the 10.7 SDK to fix!
+    if MacOS.mountain_lion? or MacOs::Xcode.version >= '4.4'
+    	header_prefix = File.join(lion_prefix, "Developer")
+    end
+    inreplace %w(fontforge/macbinary.c fontforge/startui.c gutils/giomime.c) do |s|
+		s.gsub! "/Developer", header_prefix
     end
 
     system "make"
@@ -41,7 +66,7 @@ class Fontforge < Formula
     To install the Mac OS X wrapper application run:
         brew linkapps
     or:
-        sudo ln -s #{prefix}/FontForge.app /Applications
+        ln -s #{prefix}/FontForge.app /Applications
     EOS
   end
 end
